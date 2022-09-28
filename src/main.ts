1ca4daebcaa86dac9c20bc80b5a69d01b17754b5
frazazel <sketchysolid@gmail.com>
2022-09-27 20:35:30 -0500

    softcore perm all permable skills

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,33 @@
-import { runChoice, visitUrl } from "kolmafia";
-import { $class, $item, ascend, Lifestyle, Paths, set } from "libram";
+import { getPermedSkills, handlingChoice, runChoice, toSkill, visitUrl } from "kolmafia";
+import { $skills, $class, $item, ascend, Lifestyle, Paths, set, have } from "libram";
+
 
 export function main(): void {
-  ascend(
-    Paths.GreyYou,
-    $class`Grey Goo`,
-    Lifestyle.softcore,
-    "vole",
-    $item`astral six-pack`,
-    $item`astral pet sweater`
-  );
 
-  set("choiceAdventure1464", 1);
-  if (visitUrl("main.php").includes("somewhat-human-shaped mass of grey goo nanites"))
-    runChoice(-1);
+	var skillsToPerm = new Map();
+	$skills``.forEach((sk) => {
+		if(have(sk) && sk.permable)
+			skillsToPerm.set(sk, Lifestyle.softcore);
+	});
+	Object.entries(getPermedSkills()).forEach(([sk,]) => {
+		skillsToPerm.delete(toSkill(sk));
+	});
+
+	ascend(
+		Paths.GreyYou,
+		$class`Grey Goo`,
+		Lifestyle.softcore,
+		"vole",
+		$item`astral six-pack`,
+		$item`astral mask`,
+		skillsToPerm
+	);
+
+//	set("choiceAdventure1464", 1);
+//	if (visitUrl("main.php").includes("somewhat-human-shaped mass of grey goo nanites"))
+//		runChoice(-1);
+
+	visitUrl("main.php");
+	if(handlingChoice()) //this is not robust error-checking!
+		runChoice(-1); 
 }
